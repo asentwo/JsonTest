75db32853f5453a58069af5dfaa3c376cf284e96
JD <asentwo@hotmail.com>
2016-05-14 15:23:20 -0500

    set up storyboard

--- JSON Test/ViewController.swift
+++ JSON Test/ViewController.swift
@@ -10,15 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    let apiKey = "rKk09BXyG0kXF1lnde9GOltFq6FfvNQd"
-      var showToDetailSite: NSNumber = 0
+    private let apiKey = "rKk09BXyG0kXF1lnde9GOltFq6FfvNQd"
+  let JSONParser = JSONConnect()
   
+  
+  
+  @IBOutlet weak var show1Image: UIImageView!
+  @IBOutlet weak var show2image: UIImageView!
+  @IBOutlet weak var show3image: UIImageView!
+  @IBOutlet weak var show4image: UIImageView!
+  
+  
+  @IBOutlet weak var show1label: UILabel!
+  @IBOutlet weak var show2label: UILabel!
+  @IBOutlet weak var show3label: UILabel!
+  @IBOutlet weak var show4label: UILabel!
+  
+
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
     
-   //    let newURL = "http://api-public.guidebox.com/v1.43/us/\(apiKey)/show/\(showToDetailSite)"
-   //     getJSON(newURL)
+    let newURL = "http://api-public.guidebox.com/v1.43/us/\(apiKey)/channels/all/0/4"
+   JSONParser.getJSON(newURL)
     
     
   }
